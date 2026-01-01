a047051b3b0701a5dbbd086615a4b2cffafcd78c
Tom Brandenburg <40181002+tbrandenburg@users.noreply.github.com>
2026-01-01 15:46:35 +0100

    Fix lint by removing unused React import in modal test

--- packages/frontend/src/components/ClearSessionModal.test.tsx
+++ packages/frontend/src/components/ClearSessionModal.test.tsx
@@ -2,7 +2,6 @@
 
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
-import React from "react";
 import { ClearSessionModal } from "./ClearSessionModal";
 
 describe("ClearSessionModal", () => {
