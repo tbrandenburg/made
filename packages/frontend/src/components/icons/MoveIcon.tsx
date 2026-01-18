18276153b941de3b37283e9eef4042d01193efed
Tom Brandenburg <40181002+tbrandenburg@users.noreply.github.com>
2026-01-18 11:36:21 +0100

    Add SVG file action icons

---
+++ packages/frontend/src/components/icons/MoveIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const MoveIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M5 12h14" />
+    <path d="m9 8-4 4 4 4" />
+    <path d="m15 8 4 4-4 4" />
+  </svg>
+);
